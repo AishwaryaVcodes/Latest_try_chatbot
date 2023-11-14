b0bcac7ccd230173d7dcd55e9781f285511a070f
AishwaryaVcodes <aishwaryav825@gmail.com>
2023-11-14 11:46:41 +0530

    nov14 changes

--- training.py
+++ training.py
@@ -68,7 +68,7 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-hist = model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+hist = model.fit(train_x, train_y, epochs=300, batch_size=5, verbose=1)
 model.save('model.h5')
 
 print("model created")
